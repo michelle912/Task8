e4493dcaad76c2ae91714b50e33fe1bf4c0ac9b9
Michelle <81959777+michelle912@users.noreply.github.com>
2021-11-22 09:21:32 +0800

    Create Student class

---
+++ src/pkg1/Main.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class Main {
+    public static void main(String[] args) {
+    Student student1 = new Student("John", 25, 'M', "001");
+
+        System.out.println(student1.toString());
+
+    }
+}
---
+++ src/pkg1/Student.java
@@ -0,0 +1,57 @@
+package pkg1;
+
+public class Student {
+    private String name;
+    private int age;
+    private char gender;
+    private String id;
+
+    public Student(String name, int age, char gender, String id) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
